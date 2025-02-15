e96205adea6ee3199dab89482d4724a2cc4c11d9
Norskiy <112186856+NorskiyXVIII@users.noreply.github.com>
2025-02-15 12:22:24 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-#include <cstdlib>
 #include <stdexcept>
-#include <cstring>
-#include <vector>
 #include "arena.hpp"
-#include <clocale>
 
 
 
@@ -12,26 +8,23 @@
 int main() {
     setlocale(LC_ALL, "Rus");
     try {
-        arena_allocator arena{1024};
-        int* my_array = static_cast<int*>(arena.alloc(sizeof(int) * 10));
-        short* my_array2 = static_cast<short*>(arena.alloc(sizeof(short) * 256));
-        arena_allocator::memset(arena, 'x');
-        for (int i = 0; i < 10; i++) my_array[i] = i * i;
-        //for (int i = 0; i < 10; i++) std::cout << my_array[i] << std::endl;
-        
-        for (int i = 0; i < 256; i++) my_array2[i] = i * 10;
-        //for (int i = 0; i < 512; i++) std::cout << my_array2[i] << std::endl;
-        arena.free();
-        log_allocator(arena);
-        
-        arena.deinit();
-        /*log_allocator(arena);
-        arena.init(2048);
-        log_allocator(arena);
-        */
+        arena::allocator arena{1024};
+        arena::print_arena_bytes(arena);
+
+        int* x = (int*)arena.alloc(4);
+        *x = 12894;
+        std::cout << *x << std::endl;
+
+
+        int* y = (int*)arena.alloc(sizeof(int) * 25);
+        for (int i = 0; i < 25; i++) {
+            y[i] = i * 10;
+        }
+        arena::print_arena_bytes(arena);
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << "\n";
     }
 
     return 0;
 }
+
